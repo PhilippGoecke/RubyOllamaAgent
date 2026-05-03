d89274fcf58cba773414dcae08ff17a13b4c94f4
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-05-03 22:29:05 +0200

    Update podman-run-rubyagent.bash

--- podman-run-rubyagent.bash
+++ podman-run-rubyagent.bash
@@ -5,8 +5,9 @@ podman build --no-cache --rm --file Containerfile --tag rubyagent:demo .
 # Test if models are present, if not pull them
 for model in $PLANNER_MODEL $CODER_MODEL; do
   if ! ollama list | grep -q "$model"; then
+    echo "pulling Model '$model'"
     ollama pull "$model"
   fi
 done
 mkdir -p $(pwd)/workspace
-podman run --interactive --tty --volume $(pwd)/workspace:/rubyagent/workspace --env PLANNER_MODEL --env CODER_MODEL rubyagent:demo
+podman run --interactive --tty --volume $(pwd)/workspace:/rubyagent/workspace --env PLANNER_MODEL --env CODER_MODEL --env AGENT_TASK="Write a poem to a text file" rubyagent:demo
